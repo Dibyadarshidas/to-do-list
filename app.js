03c1f3c13bf8a4191c568d15a9fb7664ffec7d0f
Dibyadarshi Das <ddas270@gmail.com>
2020-11-20 00:31:49 +0530

    v2

--- app.js
+++ app.js
@@ -1,41 +1,35 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejsLint = require('ejs-lint');
 const app = express();
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+var items = [];
 app.get("/", function(req, res) {
   var today = new Date();
   var currentDay = today.getDay();
-  var day = "";
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-    console.log("Error")
-
+  var options = {
+    weekday : "long",
+    day : "numeric",
+    month : "long"
   }
+  var day = today.toLocaleDateString("en-us", options);
   res.render("list", {
-    kindofDay: day
+    kindofDay: day,
+    newListItems : items
   })
 });
+
+app.post("/",function(req,res){
+  var item = req.body.newItem;
+  console.log(item);
+  items.push(item);
+  res.redirect("/");
+})
+
+
+
+
 app.listen(3000, function() {
   console.log("Server started on port 3000")
 });
