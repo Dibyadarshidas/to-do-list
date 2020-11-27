93262855e358b0ed2e07bf6bc96d514b4ab5f075
Dibyadarshi Das <ddas270@gmail.com>
2020-11-27 23:40:53 +0530

    final

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const ejsLint = require('ejs-lint');
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 let items = [];
+let workItems = [];
 app.get("/", function(req, res) {
   let today = new Date();
   let currentDay = today.getDay();
@@ -15,18 +17,37 @@ app.get("/", function(req, res) {
   }
   let day = today.toLocaleDateString("en-us", options);
   res.render("list", {
-    kindofDay: day,
+    listTitle: day,
     newListItems : items
   })
 });
 
 app.post("/",function(req,res){
+  if(req.body.list == "work"){
+    workItems.push(item);
+    res.redirect("/work");
+  }else{
+    items.push(item);
+    res.redirect("/");
+  }
   var item = req.body.newItem;
   console.log(item);
   items.push(item);
   res.redirect("/");
 })
 
+app.get("/work", function(req, res){
+  res.render("list",{
+    listTitle: "work",
+    newListItems : workItems
+  });
+});
+
+app.post("/work", function(req,res){
+  let item = req.body.newItem;
+  workItem.push(item);
+  res.redirect("/work");
+})
 
 
 
